33555add8b7bd9ddd116816721434f76aaa5eed2
Junichi Akita <akita@ifdl.jp>
2023-05-23 14:24:04 +0900

    initial commit

---
+++ FW_PIO/Stackchan_bit/src/Stackchan_bit.ino
@@ -0,0 +1,254 @@
+#include <Arduino.h>
+
+#include <M5Unified.h>
+#if defined(ARDUINO_M5STACK_Core2)
+  #define SERVO_PIN_X 32
+  #define SERVO_PIN_Y 27
+  #define UART_PIN_TXD 14
+  #define UART_PIN_RXD 13
+#elif defined(ARDUINO_M5Stack_Core_ESP32)
+  #define SERVO_PIN_X 12
+  #define SERVO_PIN_Y 2
+  #define UART_PIN_TXD 17
+  #define UART_PIN_RXD 16
+#endif
+
+#define X_MIN -90  // [deg]
+#define X_MAX 90   // [deg]
+#define Y_MIN 0    // [deg]
+#define Y_MAX 30   // [deg]
+#define E_MIN 0    // emotion num
+#define E_MAX 6    // emotion num
+
+int servo_offset_x = 0;  // X軸サーボのオフセット（90°からの+-で設定）
+int servo_offset_y = 0;  // Y軸サーボのオフセット（90°からの+-で設定）
+
+#include <Avatar.h> // https://github.com/meganetaaan/m5stack-avatar
+#include <ServoEasing.hpp> // https://github.com/ArminJo/ServoEasing       
+#include "formatString.hpp" // https://gist.github.com/GOB52/e158b689273569357b04736b78f050d6
+
+#define RX_BUF_SIZE 32
+char RXbuf[RX_BUF_SIZE];
+uint8_t pRX = 0;
+
+using namespace m5avatar;
+Avatar avatar;
+
+const Expression expressions[] = {
+  Expression::Angry,
+  Expression::Sleepy,
+  Expression::Happy,
+  Expression::Sad,
+  Expression::Doubt,
+  Expression::Neutral
+};
+
+#define START_DEGREE_VALUE_X 90
+#define START_DEGREE_VALUE_Y 90
+
+ServoEasing servo_x;
+ServoEasing servo_y;
+
+uint32_t mouth_wait = 2000;  // 通常時のセリフ入れ替え時間（msec）
+uint32_t last_mouth_millis = 0;
+
+const char* lyrics[] = { "BtnA:MoveTo90  ", "BtnB:ServoTest  ", "BtnC:RandomMode  ", "BtnALong:AdjustMode" };
+const int lyrics_size = sizeof(lyrics) / sizeof(char*);
+int lyrics_idx = 0;
+
+void moveX(int x, uint32_t millis_for_move = 0) {
+  if (millis_for_move == 0) {
+    servo_x.easeTo(x + servo_offset_x);
+  } else {
+    servo_x.easeToD(x + servo_offset_x, millis_for_move);
+  }
+}
+
+void moveY(int y, uint32_t millis_for_move = 0) {
+  if (millis_for_move == 0) {
+    servo_y.easeTo(y + servo_offset_y);
+  } else {
+    servo_y.easeToD(y + servo_offset_y, millis_for_move);
+  }
+}
+
+void moveXY(int x, int y, uint32_t millis_for_move = 0) {
+  if (millis_for_move == 0) {
+    servo_x.setEaseTo(x + servo_offset_x);
+    servo_y.setEaseTo(y + servo_offset_y);
+  } else {
+    servo_x.setEaseToD(x + servo_offset_x, millis_for_move);
+    servo_y.setEaseToD(y + servo_offset_y, millis_for_move);
+  }
+  // サーボが停止するまでウェイトします。
+  synchronizeAllServosStartAndWaitForAllServosToStop();
+}
+
+void adjustOffset() {
+  // サーボのオフセットを調整するモード
+  servo_offset_x = 0;
+  servo_offset_y = 0;
+  moveXY(90, 90);
+  bool adjustX = true;
+  for (;;) {
+    M5.update();
+    if (M5.BtnA.wasPressed()) {
+      // オフセットを減らす
+      if (adjustX) {
+        servo_offset_x--;
+      } else {
+        servo_offset_y--;
+      }
+    }
+    if (M5.BtnB.pressedFor(2000)) {
+      // 調整モードを終了
+      break;
+    }
+    if (M5.BtnB.wasPressed()) {
+      // 調整モードのXとYを切り替え
+      adjustX = !adjustX;
+    }
+    if (M5.BtnC.wasPressed()) {
+      // オフセットを増やす
+      if (adjustX) {
+        servo_offset_x++;
+      } else {
+        servo_offset_y++;
+      }
+    }
+    moveXY(90, 90);
+
+    std::string s;
+
+    if (adjustX) {
+      s = formatString("%s:%d:BtnB:X/Y", "X", servo_offset_x);
+    } else {
+      s = formatString("%s:%d:BtnB:X/Y", "Y", servo_offset_y);
+    }
+    avatar.setSpeechText(s.c_str());
+  }
+}
+
+void moveRandom() {
+  for (;;) {
+    // ランダムモード
+    int x = random(45, 135);  // 45〜135° でランダム
+    int y = random(60, 90);   // 50〜90° でランダム
+    M5.update();
+    if (M5.BtnC.wasPressed()) {
+      break;
+    }
+    int delay_time = random(10);
+    moveXY(x, y, 1000 + 100 * delay_time);
+    delay(2000 + 500 * delay_time);
+    //avatar.setSpeechText("Stop BtnC");
+    avatar.setSpeechText("");
+  }
+}
+
+int getParam()
+{
+  return(atoi(RXbuf+1));
+}
+
+void setup() {
+  auto cfg = M5.config();
+  M5.begin(cfg);
+  Serial2.begin(115200, SERIAL_8N1, UART_PIN_RXD, UART_PIN_TXD);
+  while (!Serial2)
+    ;
+
+  if (servo_x.attach(SERVO_PIN_X,
+                     START_DEGREE_VALUE_X + servo_offset_x,
+                     DEFAULT_MICROSECONDS_FOR_0_DEGREE,
+                     DEFAULT_MICROSECONDS_FOR_180_DEGREE)) {
+    Serial.print("Error attaching servo x");
+  }
+  if (servo_y.attach(SERVO_PIN_Y,
+                     START_DEGREE_VALUE_Y + servo_offset_y,
+                     DEFAULT_MICROSECONDS_FOR_0_DEGREE,
+                     DEFAULT_MICROSECONDS_FOR_180_DEGREE)) {
+    Serial.print("Error attaching servo y");
+  }
+  servo_x.setEasingType(EASE_QUADRATIC_IN_OUT);
+  servo_y.setEasingType(EASE_QUADRATIC_IN_OUT);
+  setSpeedForAllServos(60);
+  avatar.init();
+  last_mouth_millis = millis();
+  //moveRandom();
+}
+
+void loop() {
+  M5.update();
+  if (M5.BtnA.pressedFor(2000)) {
+    // サーボのオフセットを調整するモードへ
+    adjustOffset();
+  }
+  if (M5.BtnA.wasPressed()) {
+    moveXY(90, 90);
+  }
+
+  if (M5.BtnB.wasPressed()) {
+    for (int i = 0; i < 2; i++) {
+      avatar.setSpeechText("X 90 -> 0  ");
+      moveX(0);
+      avatar.setSpeechText("X 0 -> 180  ");
+      moveX(180);
+      avatar.setSpeechText("X 180 -> 90  ");
+      moveX(90);
+      avatar.setSpeechText("Y 90 -> 50  ");
+      moveY(50);
+      avatar.setSpeechText("Y 50 -> 90  ");
+      moveY(90);
+    }
+  }
+  if (M5.BtnC.wasPressed()) {
+    // ランダムモードへ
+    moveRandom();
+  }
+
+  if ((millis() - last_mouth_millis) > mouth_wait) {
+    const char* l = lyrics[lyrics_idx++ % lyrics_size];
+    avatar.setSpeechText(l);
+    avatar.setMouthOpenRatio(0.7);
+    delay(200);
+    avatar.setMouthOpenRatio(0.0);
+    last_mouth_millis = millis();
+  }
+  if (Serial2.available()) {
+    char c = Serial2.read();
+    RXbuf[pRX++] = c;
+    if (c == '\n' || pRX >= RX_BUF_SIZE - 1) {
+      RXbuf[pRX] = '\0';
+      pRX = 0;
+      Serial.println(RXbuf);
+      int param;
+      switch (RXbuf[0]) {
+        case 'X':
+          param = getParam();
+          if (param < X_MIN) param = X_MIN;
+          if (param > X_MAX) param = X_MAX;
+          moveX(param);
+          break;
+        case 'Y':
+          param = getParam();
+          if (param < Y_MIN) param = Y_MIN;
+          if (param > Y_MAX) param = Y_MAX;
+          moveY(param);
+          break;
+        case 'T':
+          avatar.setSpeechText(RXbuf + 1);
+          avatar.setMouthOpenRatio(0.7);
+          delay(200);
+          avatar.setMouthOpenRatio(0);
+          break;
+        case 'E':
+          param = getParam();
+          if (param < E_MIN) param = E_MIN;
+          if (param > E_MAX) param = E_MAX;
+          avatar.setExpression(expressions[param]);
+          break;
+      }
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Stack-chan_bit
-Stack-chan + micro:bit
+
+[ｽﾀｯｸﾁｬﾝ](https://protopedia.net/prototype/2345Stack-chan)に[micro:bit](https://www.switch-science.com/products/7952)を差し込んで制御するボードです。micro:bitのプログラミングでｽﾀｯｸﾁｬﾝを動かすことができます。→[使用例](https://twitter.com/akita11/status/1656496746321842176)
+
+<img src="https://github.com/akita11/Stack-chan_bit/blob/main/stack-chan_bit1.jpg" width="240px">
+
+<img src="https://github.com/akita11/Stack-chan_bit/blob/main/stack-chan_bit1.jpg" width="240px">
+
+
+## 作り方
+
+### 用意するもの
+- M5Stack (BasicまたはCore2がオススメ)
+- [Stack-chan_Takao_Base](https://github.com/akita11/Stack-chan_Takao_Base)とそれで用いるもの（サーボモータ2個、Groveケーブル、ネジ等、筐体(shell部以外)）
+- shell部の筐体(Stack-chan_bit_shell.stlを3Dプリントしたもの): [タカオ版筐体](https://github.com/meganetaaan/stack-chan/tree/dev/v1.0/case/contributed/mongonta_case_for_SG90_and_M5GoBottomBoard/case_for_SG90andM5GoBottomBoard)内の”shell_SG90_basicgraybottom_v1.0.stl"を元に改変
+
+### 組み立て
+- M5Stackに専用プログラムを書き込む（FW_PIO/以下。PlatformIO用）
+- M5Stackの底板を外し、Stack-chan_bitをさしこむ
+- [Stack-chan_Takao_Base](https://github.com/akita11/Stack-chan_Takao_Base)を使ってｽﾀｯｸﾁｬﾝを組み立てる。Stack-chan_Takao_BaseからのGroveケーブルは、M5Stack本体ではなく、Stack-chan_bitのGroveコネクタに接続する
+
+
+## プログラミング
+
+micro:bitとｽﾀｯｸﾁｬﾝ(M5Stack)の間はUART通信で以下のような制御コマンドを送って制御します。[プロトコル詳細とmicro:bit側のカスタムブロック](https://docs.google.com/document/d/167KkDTLl4q2A9jWGB8BHNa3lgDhTbEqt-1NPs4zDYiw/edit#heading=h.bze9zta2801)
+
+- 動作の制御
+ - Xnn : パン動作（横方向）nn=角度、範囲＝-90〜90（正面=0）
+ - Ynn : チルト動作（縦方向）nn=角度、範囲=0〜30（正面=0から上向きへ）
+- 顔の制御
+ - Txxxx : 吹き出しに”xxxx”と表示し、それにあわせて口が動く
+ - En : 表情を設定。n=0〜5 (0=Angry, 1=Sleepy, 2=Happy, 3=Sat, 4=Doubt, 5=Neutral)
+
+
+## Author
+
+Junichi Akita (@akita11, akita@ifdl.jp)
+
+
